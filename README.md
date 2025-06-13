394b7840c5f950a7605e87e3a6389a00da7460a6
RohitMunda07 <rohitkumar14818@gmail.com>
2025-06-13 18:12:29 +0530

    connect to mongoDb

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Node-Project
+# Always use try-catch syntax
+# Use Async-await
+# Database is in another continent
---
+++ db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constant.js";
+
+const connectDB = async () => {
+
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        console.log(connectionInstance.connection.host);
+        console.log(`\n MongoDB Connect Successfully!! DB HOST: ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.error("ERROR Connecting DB: ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
